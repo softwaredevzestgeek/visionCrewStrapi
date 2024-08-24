4278c40775a684fa5c6f94a059d650de6982e531
softwaredevzestgeek <softwaredev@zestgeek.com>
2024-08-24 17:52:36 +0530

    PORT Intialization

--- config/server.js
+++ config/server.js
@@ -1,10 +1,28 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
+//////////////////////////////////////////////////////////////////////////////
+
+
+module.exports = ({ env }) => {
+  // Log the port for debugging
+  console.log(`Starting Strapi on port ${env.int('PORT', 1337)}`);
+
+  return {
+    host: env('HOST', '0.0.0.0'), // Ensure Strapi listens on all network interfaces
+    port: env.int('PORT', 1337), // Use environment variable PORT if set
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  };
+};

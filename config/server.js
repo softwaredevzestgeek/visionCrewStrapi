e79bfcea4a1f62ab51be582459c360dcce649141
softwaredevzestgeek <softwaredev@zestgeek.com>
2024-08-26 11:31:46 +0530

    fix conflicts

--- config/server.js
+++ config/server.js
@@ -8,12 +8,7 @@
 //     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
 //   },
 // });
-<<<<<<< Updated upstream
 //////////////////////////////////////////////////////////////////////////////
-=======
-
-///////////////////////////////////////////////////////////////////////////
->>>>>>> Stashed changes
 
 
 module.exports = ({ env }) => {
@@ -22,11 +17,7 @@ module.exports = ({ env }) => {
 
   return {
     host: env('HOST', '0.0.0.0'), // Ensure Strapi listens on all network interfaces
-<<<<<<< Updated upstream
-    port: env.int('PORT', 1337), // Use environment variable PORT if set
-=======
     port: env.int('PORT', process.env.PORT || 1337), // Use environment variable PORT if set
->>>>>>> Stashed changes
     app: {
       keys: env.array('APP_KEYS'),
     },

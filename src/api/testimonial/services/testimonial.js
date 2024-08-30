5857d6fda8d91d38869949f72b4eea0976cf8030
softwaredevzestgeek <softwaredev@zestgeek.com>
2024-08-30 17:11:39 +0530

    Pagination

--- src/api/testimonial/services/testimonial.js
+++ src/api/testimonial/services/testimonial.js
@@ -4,32 +4,6 @@
  * testimonial service
  */
 
-// const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require('@strapi/strapi').factories;
 
-// module.exports = createCoreService('api::testimonial.testimonial');
-
-
-
-///////////////////////////////////////////////////////////////
-
-const { sanitize } = require('@strapi/utils');
-
-module.exports = {
-    async find(ctx) {
-        const entities = await strapi.entityService.findPage('api::testimonial.testimonial', {
-            populate: {
-                '*': {
-                    populate: '*'
-                },
-                Avatar: {
-                    populate: ['url']
-                }
-            }
-        });
-
-        const sanitizedEntities = await sanitize.contentAPI.output(entities, strapi.getModel('api::testimonial.testimonial'));
-
-        return sanitizedEntities;
-    },
-};
-
+module.exports = createCoreService('api::testimonial.testimonial');
